5392bb23ad4c79367f26243860eb32c99800e4a8
1mjCee <cjmatheka@gmail.com>
2026-01-26 07:58:57 +0300

    udate github and theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,8 @@ export const metadata: Metadata = {
     "Web Development",
   ],
   authors: [{ name: "Julius Matheka Charles" }],
+  metadataBase: new URL("https://www.juliusmatheka.space/"),
+  robots: "index, follow",
   openGraph: {
     title: "Julius Matheka Charles | Software Engineer",
     description:
@@ -28,12 +30,21 @@ export const metadata: Metadata = {
     siteName: "Julius Matheka Portfolio",
     locale: "en_US",
     type: "website",
+    images: [
+      {
+        url: "/logo/logo.png",
+        width: 1200,
+        height: 630,
+        alt: "Julius Matheka Portfolio Logo",
+      },
+    ],
   },
   twitter: {
     card: "summary_large_image",
     title: "Julius Matheka Charles | Software Engineer",
     description:
       "Building intelligent solutions through scalable web applications and machine learning integration",
+    images: ["/logo/logo.png"],
   },
 };
 
